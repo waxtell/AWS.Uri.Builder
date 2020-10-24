15314b45680aa581e50fc345b5263034a43f73ee
Will Axtell <waxtell@transtar1.com>
2020-10-24 14:12:10 -0400

    Initial concept...  lots of work to do.

---
+++ AWS.Uri.Builder.Tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace AWS.Uri.Builder.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ AWS.Uri.Builder/CloudWatchLogUriBuilder.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace AWS.Uri.Builder
+{
+    public class CloudWatchLogUriBuilder
+    {
+        public enum TimeZone
+        {
+            Local,
+            UTC
+        }
+
+        private readonly string _region;
+        private DateTime? _start;
+        private DateTime? _end;
+        private TimeZone? _timeZone;
+        private string _query;
+        private string[] _logGroups;
+        private string _timeType;
+        private bool _liveTail;
+
+        private CloudWatchLogUriBuilder(string region)
+        {
+            _region = region;
+        }
+
+        public static CloudWatchLogUriBuilder FromRegion(string region)
+        {
+            return new CloudWatchLogUriBuilder(region);
+        }
+
+        public CloudWatchLogUriBuilder WithLiveTail(bool liveTail)
+        {
+            _liveTail = liveTail;
+
+            return this;
+        }
+
+        public CloudWatchLogUriBuilder WithLogGroups(params string[] logGroups)
+        {
+            _logGroups = logGroups;
+
+            return this;
+        }
+
+        public CloudWatchLogUriBuilder WithAbsoluteRange(DateTime start, DateTime end, TimeZone timeZone)
+        {
+            _start = start;
+            _end = end;
+            _timeZone = timeZone;
+            _timeType = "ABSOLUTE";
+
+            return this;
+        }
+
+        public CloudWatchLogUriBuilder WithQuery(string query)
+        {
+            _query = query;
+
+            return this;
+        }
+
+        public System.Uri Build()
+        {
+            var builder =
+                new StringBuilder(
+                    $"https://{_region}.console.aws.amazon.com/cloudwatch/home?region={_region}#logsV2:logs-insights");
+
+            builder.Append(ToHexString("?queryDetail=", "$", true));
+
+            builder.Append(Escape("=("));
+
+            if (_end.HasValue && _start.HasValue && _timeZone.HasValue)
+            {
+                builder
+                    .Append
+                    (
+                        Escape($"end='{ToHexString(_end.Value.ToString("yyyy-MM-dd'T'HH:mm:ss.fffZ"))}=start='{ToHexString(_start.Value.ToString("yyyy-MM-dd'T'HH:mm:ss.fffZ"))}=timeType='{_timeType}=tz='{Enum.GetName(typeof(TimeZone), _timeZone.Value)}")
+                    );
+            }
+
+            if (!string.IsNullOrEmpty(_query))
+            {
+                builder.Append(Escape("=editorString='"));
+                builder.Append(ToHexString($"{_query}\n", "*", false, false));
+            }
+
+            builder.Append(Escape($"=isLiveTail={_liveTail.ToString().ToLower()}"));
+
+            if (_logGroups != null && _logGroups.Any())
+            {
+                builder
+                    .Append
+                    (
+                        Escape($"=source=(='{string.Join("='", _logGroups.Select(logGroup => ToHexString(logGroup)))})")
+                    );
+            }
+
+            builder.Append(Escape(")"));
+
+            return 
+                new System.Uri(builder.ToString());
+
+            static string Escape(string src)
+            {
+                return
+                    ToHexString
+                    (
+                        src
+                            .Replace("=", "%~")
+                            .Replace("'", "%'")
+                            .Replace("(", "%(")
+                            .Replace(")", "%)")
+                        , "$",
+                        true
+                    );
+            }
+
+            static string ToHexString(string source, string prefix = "*", bool upperCase = false, bool escapeConsecutive = true)
+            {
+                static bool IsSpecial(char c)
+                {
+                    return
+                        !(
+                            char.IsLetterOrDigit(c) ||
+                            (c == '_') ||
+                            (c == '-') ||
+                            (c == '*') ||
+                            (c == '.')
+                        );
+                }
+
+                var sb = new StringBuilder();
+
+                var isConsecutive = false;
+
+                foreach (var c in source)
+                {
+                    if (IsSpecial(c))
+                    {
+                        if (isConsecutive && escapeConsecutive)
+                        {
+                            isConsecutive = false;
+                        }
+                        else
+                        {
+                            sb.Append(prefix);
+                            isConsecutive = true;
+                        }
+
+                        sb.Append(((int)c).ToString(upperCase ? "X2" : "x2"));
+                    }
+                    else
+                    {
+                        isConsecutive = false;
+                        sb.Append(c);
+                    }
+                }
+
+                return
+                    sb.ToString();
+            }
+        }
+    }
+}
