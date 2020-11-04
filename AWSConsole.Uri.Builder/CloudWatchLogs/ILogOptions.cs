05f6b583762a157508b90da8a8fc788833811f6f
Will Axtell <waxtell@transtar1.com>
2020-11-04 14:13:43 -0500

    Added more /// comments.

--- AWSConsole.Uri.Builder/CloudWatchLogs/ILogOptions.cs
+++ AWSConsole.Uri.Builder/CloudWatchLogs/ILogOptions.cs
@@ -6,11 +6,11 @@
         /// Restrict filters, etc. to the provided log group.
         /// </summary>
         /// <param name="logGroup">Searches, etc. will apply exclusively to this log group.</param>
-        /// <returns></returns>
+        /// <returns>An ILogGroupOptions which will allow for additional specificity.</returns>
         ILogGroupOptions WithLogGroup(string logGroup);
-        
+
         /// <summary>
-        /// Search for log groups that match the provided name filter.
+        /// Search for log groups that match the provided name filter.  Please see https://docs.aws.amazon.com/AmazonCloudWatch/latest/logs/FilterAndPatternSyntax.html for filter and pattern syntax.
         /// </summary>
         /// <param name="logGroupNameFilter">Select log groups that match the name filter.</param>
         /// <returns>The only thing left to do now is Build()</returns>
--- AWSConsole.Uri.Builder/CloudWatchLogs/ILogStreamOptions.cs
+++ AWSConsole.Uri.Builder/CloudWatchLogs/ILogStreamOptions.cs
@@ -4,8 +4,26 @@ namespace AWSConsole.Uri.Builder.CloudWatchLogs
 {
     public interface ILogStreamOptions
     {
+        /// <summary>
+        /// Restrict the result logs to those that fall between the provided start and end date.
+        /// </summary>
+        /// <param name="start">Start date/time in UTC</param>
+        /// <param name="end">End date/time in UTC</param>
+        /// <returns>An ILogStreamOptions which will allow for additional log specificity.</returns>
         ILogStreamOptions WithAbsoluteRange(DateTime start, DateTime end);
+
+        /// <summary>
+        /// Restrict the result logs to those that precede the page load by the provided milliseconds value.
+        /// </summary>
+        /// <param name="lastMilliseconds">The number of milliseconds to consider when restricting logs.</param>
+        /// <returns>An ILogStreamOptions which will allow for additional log specificity.</returns>
         ILogStreamOptions WithRelativeRangeMilliseconds(uint lastMilliseconds);
+
+        /// <summary>
+        /// Provide a filter to only consider log entries that conform.  Please see https://docs.aws.amazon.com/AmazonCloudWatch/latest/logs/FilterAndPatternSyntax.html for filter and pattern syntax.
+        /// </summary>
+        /// <param name="filter"></param>
+        /// <returns>An ILogStreamOptions which will allow for additional log specificity.</returns>
         ILogStreamOptions WithFilter(string filter);
 
         /// <summary>
